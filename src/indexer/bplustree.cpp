9c54265daa8c4be684204b5286536e75f0bdf78f
Koushik Ramachandra <koushiksmiles@gmail.com>
2019-06-06 16:04:36 +0530

    Added licensing

--- src/indexer/bplustree.cpp
+++ src/indexer/bplustree.cpp
@@ -1,8 +1,17 @@
 /*
- * File:   bplustree.h
- * Author: koushik
+ * Copyright 2019 Koushik R <rkoushik.14@gmail.com>.
  *
- * Created on 17 September, 2010, 8:36 PM
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include "bplustree.h"
